6e12c3fa617486979b1bbff7c42e8ecc7ab3d42f
ariztegui8 <jorgeariztegui8@gmail.com>
2022-07-08 12:39:56 -0300

    probando

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const cargarAutos = document.querySelector('#filter');
 const filtro = document.querySelector('.filter')
-
+const example = document.querySelector('#example')
 
 
 document.addEventListener('DOMContentLoaded', ()=>{
@@ -15,17 +15,38 @@ function consultarJson(){
 }
 
 function selectJson(datos){
-    
-    const objeto = Object.entries(datos.cars)
-    console.log(objeto);
 
-    objeto.forEach(e =>{
-        console.log(e[0]);
+    const autos = Object.keys(datos.cars)
+    console.log(autos);
+    autos.map( car =>{
+        console.log(car);
         const option = document.createElement('option')
-        option.textContent = `${e[0]}`
+        option.textContent = `${car}`
         cargarAutos.appendChild(option)
-        console.log(Object.entries(e[1]));
     })
+
+    const company = Object.values(datos.cars)
+    console.log(company);
+    company.map(com =>{
+        const reducido = com.Company1
+        console.log(reducido);
+        const {Name} = reducido
+        console.log(Name);
+        const lista = document.createElement('li')
+        lista.textContent = `${Name}`
+        example.appendChild(lista)
+    })
+    
+    // const objeto = Object.entries(datos.cars)
+    // console.log(objeto);
+
+    // objeto.map(e =>{
+    //     console.log(e[1]);
+    //     const option = document.createElement('option')
+    //     option.textContent = `${e[0]}`
+    //     cargarAutos.appendChild(option)
+    //     console.log(Object.entries(e[1]));
+    // })
    
     
 }
